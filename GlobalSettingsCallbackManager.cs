8dfbfcefe1adc480b98c9743ef265fc815349852
ScreenVR <jjsstory@outlook.com>
2025-01-31 23:12:14 +0000

    Add files via upload

---
+++ GlobalSettingsCallbackManager.cs
@@ -0,0 +1,12 @@
+using GorillaLocomotion;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalSettingsCallbackManager : MonoBehaviour
+{
+    public Player Player;
+    public Transform playerTransform;
+    public Rigidbody playerRB;
+    public LayerMask LocomotiveLayers;
+}
